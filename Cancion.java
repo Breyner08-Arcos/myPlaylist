7abd6c60dd18f2f86fb5d65403425e1cdf063896
jhonRamirez09 <jhonjairoramirezestrada123@gmail.com>
2025-08-29 15:53:00 -0500

    Update Cancion.java

--- Cancion.java
+++ Cancion.java
@@ -1,5 +1,21 @@
 package MyPlaylist;
 
 public class Cancion {
-    
+
+private String titulo;
+private String artista;
+private String genero;
+private int duracionsegundos;
+
+public Cancion(){
+this.artista = artista;
+this.titulo = titulo;
+this.genero = genero;
+this.duracionsegundos = duracionsegundos;
+
 }
+
+public String reproducir(){
+    System.out.println("reproduciendo: "+titulo + " - " + artista);
+}
+
