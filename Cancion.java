c740fdecf66e7b98f38b226eccaccea3e6198d04
jhonRamirez09 <jhonjairoramirezestrada123@gmail.com>
2025-08-29 16:55:11 -0500

    Update Cancion.java

--- Cancion.java
+++ Cancion.java
@@ -19,3 +19,27 @@ public String reproducir(){
     System.out.println("reproduciendo: "+titulo + " - " + artista);
 }
 
+ 
+    private String titulo;
+    private String artista;
+    private String genero;
+    private int duracionSegundos;
+ 
+    public Cancion(String titulo, String artista, String genero, int duracionSegundos) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.genero = genero;
+        this.duracionSegundos = duracionSegundos;
+    }
+ 
+    public String getTitulo() {
+        return titulo;
+    }
+ 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+ 
+    public String getArtista() {
+        return artista;
+    }
